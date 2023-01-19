f6e69e9c6636ad4a50199a320174dd58b294086a
wlLima <lima14.wl@gmail.com>
2023-01-18 21:45:17 -0300

    Added styles for login page

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,10 +1,14 @@
+import { Container } from './styles'
+
 export default function Login(){
     return(
-        <div>
-            <input type="email" name="email" id="email" placeholder="Email"/>
-            <input type="password" name="password" id="password" placeholder="Senha" />
+        <Container>
+            <div>
+                <input type="email" name="email" id="email" placeholder="Email"/>
+                <input type="password" name="password" id="password" placeholder="Senha" />
+            </div>
             <button type="submit">Entrar</button>
             <a href="/register">Registrar-se</a>
-        </div>
+        </Container>
     );
 }
---
+++ src/pages/auth/login/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.form`
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    gap: 0.8rem;
+
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+`
