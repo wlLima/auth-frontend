bc09949c8bf19d357b86a2832ec7351e91117d4d
wlLima <lima14.wl@gmail.com>
2023-02-06 21:25:32 -0300

    Added title

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -4,6 +4,9 @@ export default function Login(){
     return(
         <Container>
             <div>
+                <h2>Login</h2>
+            </div>
+            <div>
                 <input type="email" name="email" id="email" placeholder="Email"/>
                 <input type="password" name="password" id="password" placeholder="Senha" />
             </div>
--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,11 +1,18 @@
+import { Container } from './styles'
+
 export default function Register(){
     return(
-        <div>
-            <input type="text" name="name" id="name" placeholder="Nome" />
-            <input type="email" name="email" id="email" placeholder="Email"/>
-            <input type="password" name="password" id="password" placeholder="Senha"/>
+        <Container>
+            <div>
+                <h2>Registro</h2>
+            </div>
+            <div>
+                <input type="text" name="name" id="name" placeholder="Nome" />
+                <input type="email" name="email" id="email" placeholder="Email"/>
+                <input type="password" name="password" id="password" placeholder="Senha"/>
+            </div>
             <button type="submit">Registrar</button>
             <a href="/">Voltar</a>
-        </div>
+        </Container>
     );
 }
