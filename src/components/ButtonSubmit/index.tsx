66c24f94543224519327784a18b6ca94d1ad4093
wlLima <lima14.wl@gmail.com>
2023-04-29 19:51:42 -0300

    feat: updated submit button

---
+++ src/components/ButtonSubmit/index.tsx
@@ -0,0 +1,7 @@
+import {Button} from './styles'
+
+export default function ButtonSubmit(props: any){
+  return(
+    <Button type="submit">{props.text}</Button>
+  )
+}
---
+++ src/components/ButtonSubmit/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  background-color: transparent;
+  border: 2px solid #ea2845;
+  padding: 10px 40px;
+  border-radius: 0.8rem;
+  font-size: 14px;
+  font-weight: bold;
+  color: #ea2845;
+
+  &:hover{
+    background-color: #ea2845;
+    color: white;
+    cursor: pointer;
+  }
+`
