5aca70262d07e79d240c096f9eef2776cdd686b1
wlLima <lima14.wl@gmail.com>
2023-05-01 17:05:18 -0300

    feat: added function to login

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,24 +1,41 @@
 import { Container, Banner, LoginForm, Title } from './styles'
 import Input from '../../../components/Input'
 import ButtonSubmit from '../../../components/ButtonSubmit';
+import { AuthService } from '../../../services/Auth/AuthService';
 
 export default function Login(){
-    return(
-			<Container>
-				<Banner>
-					teste
-				</Banner>
-				<LoginForm>
-					<div>
-						<Title>Login</Title>
-					</div>
-					<div className="input-container">
-						<Input type="email" name="email" id="email" placeholder="Email" />
-						<Input type="password" name="password" id="password" placeholder="Senha" />
-					</div>
-					<ButtonSubmit type="submit" variant="outlined" text="Entrar" />
-					<a href="/register">Registrar-se</a>
-				</LoginForm>
-			</Container>
-    );
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>){
+		event.preventDefault()
+
+		const {email, password } = document.forms[0]
+		
+		const data = {
+			email: email.value,
+			password: password.value
+		}
+		
+		AuthService.login(data).then((response) => {
+			console.log(response)
+		})
+	}
+
+	return(
+		<Container>
+			<Banner>
+				teste
+			</Banner>
+			<LoginForm onSubmit={(event) => handleSubmit(event)}>
+				<div>
+					<Title>Login</Title>
+				</div>
+				<div className="input-container">
+					<Input type="email" name="email" id="email" placeholder="Email" />
+					<Input type="password" name="password" id="password" placeholder="Senha" />
+				</div>
+				<ButtonSubmit type="submit" variant="outlined" text="Entrar" />
+				<a href="/register">Registrar-se</a>
+			</LoginForm>
+		</Container>
+	);
 }
