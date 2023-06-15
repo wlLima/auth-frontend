49bdc807ebb5d47ab9de1faad470537f4d00d0d2
wlLima <lima14.wl@gmail.com>
2023-06-15 15:58:06 -0300

    feat: create home page and add route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from '../pages/auth/login/'
 import Register from '../pages/auth/register/'
+import Layout from '../pages/layout/'
 
 export default function Router() {
     return (
       <Routes>
           <Route path="/" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
+          <Route path="/home" element={<Layout />}/>
       </Routes>
     );
   }
